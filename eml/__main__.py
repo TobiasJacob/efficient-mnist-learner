5d15fa7857dc413cc2e11be2efa5fd7a9f310345
Tobias Jacob <tobi.jacob@gmx.net>
2022-02-17 20:52:36 +0000

    Added cuda support

--- eml/__main__.py
+++ eml/__main__.py
@@ -1,3 +1,21 @@
+import os
+import hydra
+from omegaconf import OmegaConf
 import torch
 
-print("Hello")
+from eml.config import Config
+import torchvision
+
+
+@hydra.main(config_path=None)
+def my_app(cfg: Config) -> None:
+    torch.zeros((10, 10), device="cuda")
+    dataset = torchvision.datasets.FashionMNIST(
+        os.path.expanduser("~/dataset"), train=False, download=True
+    )
+    print(dataset[0])
+    print(OmegaConf.to_yaml(cfg))
+
+
+if __name__ == "__main__":
+    my_app()
---
+++ eml/networks/AutoEncoderClassifier.py
@@ -0,0 +1,6 @@
+from torch.nn import nn
+
+
+class AutoEncoderClassifier(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
