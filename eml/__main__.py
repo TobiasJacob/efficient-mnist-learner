9ef76fa228e58a37ab0cfc0e2b09cc3673913fd0
Tobias Jacob <tobi.jacob@gmx.net>
2022-02-17 21:53:52 +0000

    Implemented up down convolutions

--- eml/__main__.py
+++ eml/__main__.py
@@ -1,19 +1,33 @@
 import os
+
 import hydra
-from omegaconf import OmegaConf
 import torch
+import torchvision
+from omegaconf import OmegaConf
+from torchvision import transforms
 
 from eml.config import Config
-import torchvision
+from eml.networks.AutoEncoderClassifier import AutoEncoderClassifier
 
 
 @hydra.main(config_path=None)
 def my_app(cfg: Config) -> None:
     torch.zeros((10, 10), device="cuda")
     dataset = torchvision.datasets.FashionMNIST(
-        os.path.expanduser("~/dataset"), train=False, download=True
+        os.path.expanduser("~/dataset"),
+        train=True,
+        download=True,
+        transform=transforms.Compose([transforms.ToTensor()]),
     )
-    print(dataset[0])
+    print(len(dataset))
+    orig_train_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=100, shuffle=True
+    )
+
+    nn = AutoEncoderClassifier(image_size=(28, 28))
+    imgs, labels = next(iter(orig_train_loader))
+    print(nn(imgs).shape)
+    print(dataset[0][0].shape)
     print(OmegaConf.to_yaml(cfg))
 
 
--- eml/networks/AutoEncoderClassifier.py
+++ eml/networks/AutoEncoderClassifier.py
@@ -1,6 +1,32 @@
-from torch.nn import nn
+from typing import List, Tuple
+
+import torch
+import torch.nn as nn
 
 
 class AutoEncoderClassifier(nn.Module):
-    def __init__(self) -> None:
+    def __init__(
+        self, image_size: Tuple[int, int], channels: List[int] = [6, 10, 20]
+    ) -> None:
         super().__init__()
+        self.image_size = image_size
+        down_convolutions = []
+        for i in range(len(channels)):
+            down_convolutions.append(
+                nn.Conv2d(1 if i == 0 else channels[i - 1], channels[i], 3)
+            )
+        self.down_convolutions = nn.ModuleList(down_convolutions)
+
+        up_convolutions = []
+        for i in reversed(range(len(channels))):
+            up_convolutions.append(
+                nn.ConvTranspose2d(channels[i], 1 if i == 0 else channels[i - 1], 3)
+            )
+        self.up_convolutions = nn.ModuleList(up_convolutions)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for layer in self.down_convolutions:
+            x = layer(x)
+        for layer in self.up_convolutions:
+            x = layer(x)
+        return x
